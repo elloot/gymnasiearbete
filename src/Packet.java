d7b8c9f7a80403c2eb9bf4cb29b6839adf01c5b5
elloot <elliot.duchek@gmail.com>
2020-12-01 20:55:53 +0100

    generate id, add hashCode method

--- src/Packet.java
+++ src/Packet.java
@@ -1,14 +1,19 @@
+import java.util.Objects;
+
 public class Packet {
     private int id;
     private Router source;
     private Router destination;
     private String data;
 
-    public Packet(int id, Router source, Router destination, String data) {
-        this.id = id;
+    public Packet(Router source, Router destination, String data) {
+        // temporary id for hashing later
+        this.id = (int) (Math.random() * 1000000);
         this.source = source;
         this.destination = destination;
         this.data = data;
+
+        this.id = this.hashCode();
     }
 
     public int getId() {
@@ -21,6 +26,11 @@ public class Packet {
 
     public Router getDestination() {
         return destination;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, source, destination, data);
     }
 
     public String getData() {
