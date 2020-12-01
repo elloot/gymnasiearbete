b6ec729e13976b219da9c64fd2fce6d1c1f379a4
elloot <elliot.duchek@gmail.com>
2020-12-01 20:46:19 +0100

    add methods and variables from class diagram

--- src/Computer.java
+++ src/Computer.java
@@ -1,2 +1,17 @@
 public class Computer {
+    private Router router;
+
+    public Computer(Router router) {
+        this.router = router;
+    }
+
+    private void sendPacket(Packet packet) {
+        this.router.forwardPacket(packet);
+    }
+
+    public Packet createPacket(String data, Router destination) {
+        Packet packet = new Packet(this.router, destination, data);
+        this.sendPacket(packet);
+        return packet;
+    }
 }
