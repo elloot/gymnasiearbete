b5e89eb23ab0c92651f55a9664e51d2e5405538a
elloot <elliot.duchek@gmail.com>
2020-12-01 20:58:49 +0100

    add method for adding neighbours, add equals method, start working on forwardPacket method

--- src/Router.java
+++ src/Router.java
@@ -13,13 +13,28 @@ public class Router {
     }
 
     public void forwardPacket(Packet packet) {
+        if (packet.getDestination().equals(this)) {
+            this.handlePacket();
+        }
     }
 
     public ArrayList<Link> getNeighbours() {
         return neighbours;
     }
 
+    public void addNeighbour(Link link) {
+
+    }
+
     public int getAddress() {
         return address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Router router = (Router) o;
+        return address == router.address;
     }
 }
