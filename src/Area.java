18e172473a0f00501a1635e97ab1b7773128578e
elloot <elliot.duchek@gmail.com>
2020-12-01 20:46:10 +0100

    add methods and variables from class diagram

--- src/Area.java
+++ src/Area.java
@@ -1,2 +1,12 @@
 public class Area {
+    private int address;
+    private Router[] routers;
+
+    public Area(Router[] routers) {
+        this.routers = routers;
+    }
+
+    public Router getLvlOneTwo() {
+
+    }
 }
