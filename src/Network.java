c126c8cf212115122e1dae60c7c208b13eafdbc3
elloot <elliot.duchek@gmail.com>
2020-12-05 15:33:23 +0100

    add methods for populating Network with Areas and Routers

--- src/Network.java
+++ src/Network.java
@@ -1,10 +1,38 @@
 public class Network {
     private Area[] areas;
+    private int numAreas;
+    private int routersPerArea;
 
-    public Network() {
+    public Network(int numAreas, int routersPerArea) {
+        this.numAreas = numAreas;
+        areas = new Area[numAreas];
+        this.routersPerArea = routersPerArea;
+        this.populate();
     }
 
+    // fills the Network with Areas and subsequent Routers
     public void populate() {
+        int routerAddress = 1;
+        int areaAddress = 1;
+        for (int i = 0; i < numAreas; i++) {
+            Router[] routers = new Router[routersPerArea];
+            for (int j = 0; j < routersPerArea; j++) {
+                routers[j] = createRouter(routerAddress, j);
+                routerAddress++;
+            }
+            areas[i] = createArea(areaAddress, routers);
+            areaAddress++;
+        }
+    }
 
+    private Router createRouter(int routerAddress, int j) {
+        // creates a new router, if the router is the last one to be created
+        // in this loop,
+        // set it to be a lvl 1-2 router
+        return new Router(routerAddress, j % (routersPerArea - 1) == 0 && j != 0 ? 12 : 1);
+    }
+
+    private Area createArea(int areaAddress, Router[] routers) {
+        return new Area(areaAddress, routers);
     }
 }
