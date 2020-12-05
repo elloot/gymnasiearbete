40433883c85a686f6e49b52a6c59980b4da50bb6
elloot <elliot.duchek@gmail.com>
2020-12-05 15:34:02 +0100

    add toString method, add address to constructor, finish getLvlOneTwo method

--- src/Area.java
+++ src/Area.java
@@ -1,12 +1,29 @@
+import java.util.Arrays;
+
 public class Area {
     private int address;
     private Router[] routers;
 
-    public Area(Router[] routers) {
+    public Area(int address, Router[] routers) {
         this.routers = routers;
+        this.address = address;
     }
 
     public Router getLvlOneTwo() {
+        for (Router router : routers) {
+            if (router.getLevel() == 12) {
+                return router;
+            }
+        }
 
+        return new Router(-1, 1);
+    }
+
+    @Override
+    public String toString() {
+        return "Area{" +
+                "address=" + address +
+                ", routers=" + Arrays.toString(routers) +
+                '}';
     }
 }
