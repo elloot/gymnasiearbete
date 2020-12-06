51f0699c1bdca386f2a6a32ca3b6504e66899e66
elloot <elliot.duchek@gmail.com>
2020-12-06 16:13:32 +0100

    make lvl onetwo routers neighbours, add method for building graph

--- src/Network.java
+++ src/Network.java
@@ -1,13 +1,54 @@
+import org.jgrapht.Graph;
+import org.jgrapht.alg.interfaces.ShortestPathAlgorithm;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleWeightedGraph;
+
+import java.util.Random;
+
 public class Network {
     private Area[] areas;
     private int numAreas;
     private int routersPerArea;
+    private static Random r = new Random();
 
     public Network(int numAreas, int routersPerArea) {
         this.numAreas = numAreas;
         areas = new Area[numAreas];
         this.routersPerArea = routersPerArea;
         this.populate();
+        this.setLvlOneTwoNeighbours();
+
+        // TODO: visualise graph
+        Graph<Router, DefaultWeightedEdge> g = buildGraph();
+        DijkstraShortestPath<Router, DefaultWeightedEdge> dijkstraAlg = new DijkstraShortestPath<>(g);
+        Router r1 = areas[3].getRouters()[0];
+        Router r2 = areas[0].getRouters()[3];
+        ShortestPathAlgorithm.SingleSourcePaths<Router, DefaultWeightedEdge> rPaths = dijkstraAlg.getPaths(r1);
+
+        System.out.println(rPaths.getPath(r2));
+    }
+
+    public void setLvlOneTwoNeighbours() {
+        for (int i = 0; i < areas.length; i++) {
+            Area currentArea = areas[i];
+            Router currentOneTwo = currentArea.getLvlOneTwo();
+            Area nextArea = areas[i + 1 < areas.length ? i + 1 : 0];
+            Router nextOneTwo = nextArea.getLvlOneTwo();
+            currentArea.getLvlOneTwo().addNeighbour(createNeighbourLink(currentOneTwo, nextOneTwo));
+        }
+    }
+
+    private Link createNeighbourLink(Router r1, Router r2) {
+        return new Link(neighbourArray(r1, r2), randomWeight());
+    }
+
+    private int randomWeight() {
+        return r.nextInt(10) + 1;
+    }
+
+    private Router[] neighbourArray(Router r1, Router r2) {
+        return new Router[]{r1, r2};
     }
 
     // fills the Network with Areas and subsequent Routers
@@ -23,6 +64,25 @@ public class Network {
             areas[i] = createArea(areaAddress, routers);
             areaAddress++;
         }
+    }
+
+    public Graph<Router, DefaultWeightedEdge> buildGraph() {
+        Graph<Router, DefaultWeightedEdge> g = new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
+
+        for (Area currentArea : areas) {
+            for (Router currentRouter : currentArea.getRouters()) {
+                for (Link currentNeighbourLink : currentRouter.getNeighbours()) {
+                    Router r1 = currentNeighbourLink.getRouters()[0];
+                    Router r2 = currentNeighbourLink.getRouters()[1];
+                    g.addVertex(r1);
+                    g.addVertex(r2);
+                    g.addEdge(r1, r2);
+                    g.setEdgeWeight(r1, r2, currentNeighbourLink.getWeight());
+                }
+            }
+        }
+
+        return g;
     }
 
     private Router createRouter(int routerAddress, int j) {
